a0fee7c531275dd3a8c15fe7d02199d933e58ad8
Garrett Folks <Gfolks@easydynamics.com>
2018-10-28 12:26:36 -0400

    updated checking for existing stack and url building for template body.

--- run-cf.ps1
+++ run-cf.ps1
@@ -7,20 +7,22 @@
 # Set reusable variables.
 # This script assumes your template file is named <stack-name>.<template-format>
 
-$stackName="your-stack"                          # Name of the stack to create.
-$bucketName="your-bucket"                        # Name of the bucket where the template body is located.
-$templateFormat="json"                           # Format the template is in (json or yaml)
-$paramFile="file://your-params.json"             # File containint the parameters for your script.
-$capabilities="--capabilities your-capabilities" # The capabilities required for stack creation.
-$rollbackPolicy="--disable-rollback"             # Useful for debugging but stacks will take longer to delete.
+$stackName="your-stack"                             # Name of the cloudformation stack to create.
+$bucketName="your-bucket"                           # Name of the s3 bucket where the template body is located.
+$templateFormat="json"                              # File format of the template (json or yaml)
+$parameters="--parameters file://your-params.json"  # File containing the parameters for your script.
+$capabilities="--capabilities your-capabilities"    # The capabilities required for stack creation.
+$rollbackPolicy="--disable-rollback"                # The rollback policy to use. Disabling causes longer stack deletion times.
+$s3Prefix="https://s3.amazonaws.com"                # The s3 endpoint for the deployment region.
+$templateURL="--template-url $s3Prefix/$bucketName/$stackName.$templateFormat"
 
-# Make sure the s3 bucket has an up-to-date copy of the CF template
+# Make sure the s3 bucket has an up-to-date copy of the CF template.
 aws s3 cp ./$stackName.json s3://$bucketName/ --quiet
 Write-Output "Using most recent $stackName.$templateFormat file."
 
 # If the previous stack is still there, delete it before we continue.
 $stackExists=aws cloudformation list-stacks --query 'StackSummaries[?StackName==`'$stackName'`] | [?StackStatus!=`DELETE_COMPLETE`]' --output text
-if ($stackExists) {
+if ([string]::IsNullOrEmpty($stackExists)) {
     Write-Output "Deleting previous stack. Please wait..."
     aws cloudformation delete-stack --stack-name $stackName
     aws cloudformation wait stack-delete-complete --stack-name $stackName
@@ -33,6 +35,6 @@ else {
 Write-Output "Creating the new stack..."
 
 # ***This line may require modification depending on your specific stack.***
-aws cloudformation create-stack --stack-name $stackName --template-url https://s3.amazonaws.com/$bucketName/$stackName.$templateFormat --parameters $paramFile $capabilities
+aws cloudformation create-stack $rollbackPolicy --stack-name $stackName $templateURL --parameters $parameters $capabilities
 aws cloudformation wait create-stack-complete --stack-name $stackName
 
