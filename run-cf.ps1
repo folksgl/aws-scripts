777bba34bf0267c7f2f72ca801acd4d5116aeb4c
Garrett Folks <Gfolks@easydynamics.com>
2018-11-01 11:09:49 -0400

    added support for nested stacks and removed need to specify template format (json/yaml)

--- run-cf.ps1
+++ run-cf.ps1
@@ -8,17 +8,24 @@
 # This script assumes your template file is named <stack-name>.<template-format>
 
 $stackName="your-stack"                             # Name of the cloudformation stack to create.
+$stackTemplateFile="teamcity-setup.json"            # File containing the CF template
 $bucketName="your-bucket"                           # Name of the s3 bucket where the template body is located.
-$templateFormat="json"                              # File format of the template (json or yaml)
 $parameters="--parameters file://your-params.json"  # File containing the parameters for your script.
 $capabilities="--capabilities your-capabilities"    # The capabilities required for stack creation.
 $rollbackPolicy="--disable-rollback"                # The rollback policy to use. Disabling causes longer stack deletion times.
 $s3Prefix="https://s3.amazonaws.com"                # The s3 endpoint for the deployment region.
-$templateURL="--template-url $s3Prefix/$bucketName/$stackName.$templateFormat"
+$nestedStackNames=@("nested-stack.json")            # The names of any nested stacks used in the main stack.
+$nestedStackBuckets=@("nested-stack-bucket")        # Location of the nested stacks. Array location corresponds to stackNames array location.
 
 # Make sure the s3 bucket has an up-to-date copy of the CF template.
-aws s3 cp ./$stackName.json s3://$bucketName/ --quiet
-Write-Output "Using most recent $stackName.$templateFormat file."
+aws s3 cp ./$stackTemplateFile s3://$bucketName/ --quiet
+Write-Output "Using most recent $stackTemplateFile file."
+
+# Make sure nested stacks are up-to-date as well.
+for ($i=0; $i -lt $nestedStackNames.length; $i++) {
+    aws s3 cp $nestedStackNames[$i] s3://$nestedStackBuckets/ --quiet
+    Write-Output "Using most recent $nestedStackNames[$i] file."
+}
 
 # If the previous stack is still there, delete it before we continue.
 $stackExists=aws cloudformation list-stacks --query 'StackSummaries[?StackName==`'$stackName'`] | [?StackStatus!=`DELETE_COMPLETE`]' --output text
@@ -34,7 +41,10 @@ else {
 # Create the new stack and let the user know.
 Write-Output "Creating the new stack..."
 
+# Build the templateURL from supplied parameters to clean up call to stack-create.
+$templateURL="--template-url $s3Prefix/$bucketName/$stackTemplateFile"
+
 # ***This line may require modification depending on your specific stack.***
-aws cloudformation create-stack $rollbackPolicy --stack-name $stackName $templateURL --parameters $parameters $capabilities
+aws cloudformation create-stack $rollbackPolicy --stack-name $stackName $templateURL $parameters $capabilities
 aws cloudformation wait create-stack-complete --stack-name $stackName
 
